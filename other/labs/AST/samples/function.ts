260c04a93af62f0299061e67298e947169fbdb48
Ignacio Ferro Picón <nacho@binpar.com>
2019-07-09 10:10:24 +0200

    fix

--- other/labs/AST/samples/function.ts
+++ other/labs/AST/samples/function.ts
@@ -1,8 +1,12 @@
-/**
- * Retorna la suma de los dos operadores
- * @param a Primer sumando
- * @param b Segundo sumando
- */
-function sum(a: number, b: number): number {
-  return a + b; // Suma de los operadores
+namespace mathematics {
+  /**
+   * Retorna la suma de los dos operadores
+   * @param a Primer sumando
+   * @param b Segundo sumando
+   */
+  export function sum(a: number, b: number): number {
+    return a + b; // Suma de los operadores
+  }
 }
+
+const result = mathematics.sum(1, 2);
