493dee21e2de3bc6954490ae7b9fa275ae73c73b
Ignacio Ferro Picón <nacho@binpar.com>
2019-07-09 14:55:22 +0200

    Fix

---
+++ other/labs/AST/samples/authentication/function.ts
@@ -0,0 +1,8 @@
+/**
+ * Retorna la suma de los dos operadores
+ * @param a Primer sumando
+ * @param b Segundo sumando
+ */
+export function login(a: number, b: number): number {
+  return a + b; // Suma de los operadores
+}
