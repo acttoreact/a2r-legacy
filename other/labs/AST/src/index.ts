c6e68785209528567cc2a547f41f7f1c7be9e3c2
Ignacio Ferro Picón <nacho@binpar.com>
2019-07-09 16:54:09 +0200

    fix

--- other/labs/AST/src/index.ts
+++ other/labs/AST/src/index.ts
@@ -1,58 +1,57 @@
+/*eslint no-console: "off" */
 import fs from 'fs';
 import ts from 'typescript';
 import path from 'path';
 
 const normalizedPath = path.resolve('./samples');
-
-export interface IJSDocContainer {
-  jsDoc?: ts.JSDoc[];                      // JSDoc that directly precedes this node
-  jsDocCache?: ReadonlyArray<ts.JSDocTag>; // Cache for getJSDocTags
+interface IJSDocContainer {
+  jsDoc?: ts.JSDoc[];
+  jsDocCache?: ts.JSDocTag[];
 }
 
-const files_to_process = 2;
-
-async function processFilesPath(path: string) {
-  function visit(node: ts.Node, fullNamespace: string[] = new Array<string>()) {
+async function processFilesPath(pathToProcess: string): Promise<void> {
+  function visit(node: ts.Node, fullNamespace: string[] = new Array<string>()): void {
     if (ts.isModuleDeclaration(node)) {
       const newNamespace = [...fullNamespace, node.name.getText()];
-      node.forEachChild(node => visit(node, newNamespace));
+      node.forEachChild((subNode): void => visit(subNode, newNamespace));
     } else if (ts.isFunctionDeclaration(node)) {
-      const functionNode: ts.FunctionDeclaration = <ts.FunctionDeclaration>node;
-      const jsdocNode: IJSDocContainer = <IJSDocContainer>node;
+      const functionNode: ts.FunctionDeclaration = node as ts.FunctionDeclaration;
+      const jsdocNode: IJSDocContainer = node as IJSDocContainer;
 
       console.log(`**Function ${fullNamespace.join('.')}.${functionNode.name ? functionNode.name.getText() : 'Unnamed function'} that returns type ${functionNode.type ? functionNode.type.getText() : ''}:`);
 
-      if (jsdocNode && jsdocNode.jsDoc && jsdocNode.jsDoc.length == 1) {
-        console.log('   ' + jsdocNode.jsDoc[0].getText());
+      if (jsdocNode && jsdocNode.jsDoc && jsdocNode.jsDoc.length === 1) {
+        console.log(`   ${jsdocNode.jsDoc[0].getText()}`);
       } else {
         console.log('Undocumented!');
       }
 
-
-      for (const param of node.parameters) {
+      node.parameters.forEach((param): void => {
         console.log(`\t\tParam named ${param.name.getText()} of type ${param.type ? param.type.getText() : ''}`);
-      }
+      });
+
     } else {
-      node.forEachChild(node => visit(node, fullNamespace));
+      node.forEachChild((subNode): void => visit(subNode, fullNamespace));
     }
   }
 
   try {
-    const fileNames = await fs.promises.readdir(path);
-    const typeScriptFiles = fileNames.filter(name => name.endsWith('.ts'));
+    const fileNames = await fs.promises.readdir(pathToProcess);
+    const typeScriptFiles = fileNames.filter((name): boolean => name.endsWith('.ts'));
     await Promise.all(
-      typeScriptFiles.map(async fileName => {
-        const filePath = `${path}/${fileName}`;
+      typeScriptFiles.map(async (fileName): Promise<void> => {
+        const filePath = `${pathToProcess}/${fileName}`;
         const sourceCode = await fs.promises.readFile(filePath, 'utf-8');
         const sourceFile = ts.createSourceFile(filePath, sourceCode, ts.ScriptTarget.Latest, true);
         visit(sourceFile);
       })
     );
   } catch (ex) {
-    console.log(`Error process Files Path '${path}': ${ex}`);
+    console.log(`Error process Files Path '${pathToProcess}': ${ex}`);
   }
 }
 
-processFilesPath(normalizedPath).then(() => {
+processFilesPath(normalizedPath).then((): void => {
   console.log('OK');
 });
+  
