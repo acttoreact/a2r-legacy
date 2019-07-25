3ea21ebc7f3e02c6d168bcf66561aa35315df6bf
Ignacio Ferro Picón <nacho@binpar.com>
2019-07-25 20:05:26 +0200

    colors

--- core/src/commands/commandLineInfo.ts
+++ core/src/commands/commandLineInfo.ts
@@ -1,6 +1,7 @@
 import colors from 'colors';
 import setting from '../config/settings';
 
+
 export default [
   {
     header: 'A2R Framework',
@@ -12,18 +13,18 @@ export default [
                 -ooooooooooooooooooo/.                      -/ooooooooooooooooooooooooooooo+:.      
                .ooooooooooooooooooo\`                          .+oooooooooooooooooooooooooooooo+-    
               \`oooooooooooooooooooo/                           \`+ooooooooooooooooooooooooooooooo+\`  
-             \`+ooooooooooooooooooooo/     \`.-:///:-\`-/-         \`ooooooooooooooooooooooooooooooooo\` 
-             +oooooooooooooooooo+////\` \`-/oooooooooo+.\`          :ooooooooooo:.....-:+oooooooooooo: 
-            /ooooooooooooooo/-.\`\`....-+ooooo+-.-oooo+            .ooooooooooo-        :oooooooooooo 
-           :ooooooooooooooo+ \`/+oooooooooooo-   /ooo:            -ooooooooooo-         oooooooooooo 
-          -ooooooooooooooooo\` -/ooooooooooooo+/+ooo/             /ooooooooooo-        .ooooooooooo+ 
-         -ooooooooooooooooooo:  +oooooooooooooooo+-             .oooooooooooo-    \`\`.:oooooooooooo- 
-        .oooooooooooooooooooo. :ooooooooooooooo+-\`             \`+ooooooooooooo+++++oooooooooooooo:  
-       \`ooooooooooooooooooooo\`\`.ooooooooooooo:.               -oooooooooooooooooooooooooooooooo+-   
-      \`+oooooooooooooooooooo--o:\`/oooooooooo-               \`/ooooooooooooooooooooooooooooooo+-\`    
-      /oooooooooooooooo/..-o/.//- .-...oooo:              \`/oooooooooooooooooooooooooooooo+-\`       
-     /ooooooooooooooooo:\`  \`+o//++///- -++-            \`-/ooooooooooooooooooooooooooooooooo-        
-    :ooooooooooooooooo:\`....+ooooooooo:.\`\`.         \`-/ooooooooooooooooooooooooooooooooooooo/\`      
+             \`+ooooooooooooooooooooo/     ${colors.yellow('`.-:///:-`-/-')}         \`ooooooooooooooooooooooooooooooooo\` 
+             +oooooooooooooooooo+////\`  ${colors.yellow('`-/oooooooooo+.`')}          :ooooooooooo:.....-:+oooooooooooo: 
+            /ooooooooooooooo/-.\`\`....${colors.yellow('-+ooooo+-.-oooo+')}            .ooooooooooo-        :oooooooooooo 
+           :ooooooooooooooo+ ${colors.yellow('`/+oooooooooooo-   /ooo:')}            -ooooooooooo-         oooooooooooo 
+          -ooooooooooooooooo\` ${colors.yellow('-/ooooooooooooo+/+ooo/')}             /ooooooooooo-        .ooooooooooo+ 
+         -ooooooooooooooooooo:  ${colors.yellow('+oooooooooooooooo+-')}             .oooooooooooo-    \`\`.:oooooooooooo- 
+        .oooooooooooooooooooo. ${colors.yellow(':ooooooooooooooo+-`')}             \`+ooooooooooooo+++++oooooooooooooo:  
+       \`ooooooooooooooooooooo\`${colors.yellow('`.ooooooooooooo:.')}               -oooooooooooooooooooooooooooooooo+-   
+      \`+oooooooooooooooooooo${colors.yellow('--o:`/oooooooooo-')}               \`/ooooooooooooooooooooooooooooooo+-\`    
+      /oooooooooooooooo/..${colors.yellow('-o/.//- .-...oooo:')}              \`/oooooooooooooooooooooooooooooo+-\`       
+     /ooooooooooooooooo:\`  ${colors.yellow('`+o//++///- -++-')}            \`-/ooooooooooooooooooooooooooooooooo-        
+    :ooooooooooooooooo:${colors.yellow('`....+')}ooooooooo:.\`\`.         \`-/ooooooooooooooooooooooooooooooooooooo/\`      
    -ooooooooooooooooooooooooooooooooooooooo/      ./ooooooooooooooooooooooooooooooooooooooooo+.     
   .oooooooooooo................-oooooooooooo:    .---------------:ooooooooooo:..../oooooooooooo:    
  \`oooooooooooo-                 -oooooooooooo-                   .ooooooooooo-     -oooooooooooo+\`  
