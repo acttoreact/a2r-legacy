79c62ea88fc5c9da9fb18e46d98b5d2cd016e874
drnachio <nacho@binpar.com>
2019-08-14 15:18:29 +0200

    Legit Loop

--- core/src/commands/waitForCurrentVersion.ts
+++ core/src/commands/waitForCurrentVersion.ts
@@ -1,5 +1,3 @@
-import colors from 'colors';
-import out from '../util/out';
 import getLastVersionOfA2R from './getLastVersionOfA2R';
 import getCurrentA2RPackageInfo from './getCurrentA2RPackageInfo';
 
@@ -7,32 +5,13 @@ const getVersion = async (): Promise<void> => {
 
   const parsedA2RPackage = await getCurrentA2RPackageInfo();
 
-  const lastVersion = await getLastVersionOfA2R();
+  let lastVersion = await getLastVersionOfA2R();
 
   const currentVersion = parsedA2RPackage.version;
 
-  if (lastVersion === currentVersion) {
-    out.info(
-      colors.yellow.bold(
-        `Your project is using the last version (${colors.green(
-          currentVersion
-        )}) of the ${colors.magenta('A2R')} Framework 👌`
-      )
-    );
-  } else {
-    out.info(
-      colors.yellow.bold(
-        `Your project is using version (${colors.green(
-          currentVersion
-        )}) of the ${colors.magenta(
-          'A2R'
-        )} Framework. There is a v${colors.green(
-          lastVersion
-        )} available use ${colors.bgBlue.magenta(
-          'npx a2r ---update'
-        )} to upgrade the project.`
-      )
-    );
+  while (lastVersion !== currentVersion) {
+    // eslint-disable-next-line no-await-in-loop
+    lastVersion = await getLastVersionOfA2R();
   }
 };
 
