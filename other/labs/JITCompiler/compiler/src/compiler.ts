a4c901131b39719a5aab4b3be817983580bef3a6
Ignacio Ferro Picón <nacho@binpar.com>
2019-08-16 09:59:48 +0200

    Fix

--- other/labs/JITCompiler/compiler/src/compiler.ts
+++ other/labs/JITCompiler/compiler/src/compiler.ts
@@ -54,8 +54,6 @@ function watchMain(): void {
 
   // You can technically override any given hook on the host, though you probably
   // don't need to.
-  // Note that we're assuming `origCreateProgram` and `origPostProgramCreate`
-  // doesn't use `this` at all.
   const origCreateProgram = host.createProgram;
 
   host.createProgram = (
