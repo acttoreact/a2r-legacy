c0f440cd871b86cf08f645b5d0c2a403b0fe3ef5
drnachio <nacho@binpar.com>
2020-01-27 16:16:51 +0100

    fix

--- examples/basic/pages/_app.tsx
+++ examples/basic/pages/_app.tsx
@@ -1,9 +1,8 @@
 import { registerGlobal } from 'a2r';
 import App, { AppInitialProps, AppContext } from 'a2r/app';
 import Head from 'a2r/head';
-import React, { createContext, useContext } from 'a2r/react';
 import getData from 'a2r/api/getData';
-
+import React, { createContext, useContext } from 'react';
 import { GlobalProps, globalProvider } from '../config/data';
 
 registerGlobal(globalProvider);
@@ -13,41 +12,44 @@ const SessionContext = createContext<string>('');
 class A2RApp extends App {
   public static async getInitialProps({ ctx }: AppContext): Promise<AppInitialProps> {
     const { data, sessionId } = await getData<GlobalProps>(ctx);
-
-    console.log('Path: ', ctx.asPath, ctx.pathname, ctx.query);
-    console.log('Data: ', {...data});
-  
     return {
       pageProps: {
         sessionId,
         ...data,
       },
-    };
-  }
-
-  public render(): JSX.Element {
-    const {
-      Component,
-      pageProps: { sessionId, ...props },
-    } = this.props;
-
-    const { title } = props;
-
-    const MemoComponent = React.memo(Component);
-
-    return (
-      <>
-        <Head>
-          <title>{title}</title>
-        </Head>
-        <SessionContext.Provider value={sessionId}>
-          <MemoComponent {...props} />
-        </SessionContext.Provider>
-      </>
-    );
+    };    
   }
 }
 
 export const useSessionId = (): string => useContext(SessionContext);
 
-export default A2RApp;
+
+class MyApp extends A2RApp {
+
+  public render(): JSX.Element {    
+    const MemoComponent = React.memo(this.props.Component);
+    return (
+      <>
+        <Head>
+          <title>{this.props.pageProps.title}</title>
+        </Head>
+        <SessionContext.Provider value={this.props.pageProps.sessionId}>
+          <MemoComponent {...this.props.pageProps} />
+        </SessionContext.Provider>
+      </>
+    );
+  }
+
+}
+
+export default MyApp;
+
+
+
+
+
+
+
+
+
+
